95bbff54405a52211f375ce6d46c64104d0c7a36
tchikuba <chikuba.tsutomu@gmail.com>
2017-01-20 13:17:34 +0900

    ログアウト機能を実装&ヘルパーメソッド移管

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to login_url
   end
 end
