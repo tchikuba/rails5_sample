4b00e9f5cf433843eaa1a8525468568464df00f8
tchikuba <chikuba.tsutomu@gmail.com>
2017-04-15 22:52:06 +0900

    ログインしないとチャットにアクセスできないようにする

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -1,5 +1,9 @@
 class ChatMessagesController < ApplicationController
   def index
+    if current_user.blank?
+      session[:callback] = chat_messages_index_path
+      return redirect_to login_path
+    end
     @chat_messages = ChatMessage.all
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,8 +8,10 @@ module SessionsHelper
   end
 
   def log_in(user)
+    callback = session[:callback]
     reset_session
     session[:user_id] = user.id
+    session[:callback] = callback
   end
 
   def log_out
