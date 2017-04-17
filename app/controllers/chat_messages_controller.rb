24cc7ae5b3b1634ffe2dbe93c24eb91f068b471c
tchikuba <chikuba.tsutomu@gmail.com>
2017-04-17 19:11:58 +0900

    ヘルパーメソッドを使うように修正

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -1,6 +1,6 @@
 class ChatMessagesController < ApplicationController
   def index
-    if current_user.blank?
+    unless logged_in?
       session[:callback] = chat_messages_index_path
       return redirect_to login_path
     end
